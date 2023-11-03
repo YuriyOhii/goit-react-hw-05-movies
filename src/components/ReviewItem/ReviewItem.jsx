762c7827bd52c766309149bd7d6ba49b0c1079ca
YuriyOhii <uaogiy@gmail.com>
2023-11-03 11:52:25 +0200

    reviws without styles

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,3 +1,18 @@
-export const ReviewItem = ({}) => {
-  return <div>review Item</div>;
+export const ReviewItem = ({ review: { author, content, updated_at } }) => {
+  const date = new Date(updated_at)
+  return (
+    <div>
+      <div>
+        <p>
+          <b>Author: </b>
+          {author}
+        </p>
+        <p>
+          <b>Date: </b>
+          {date.toDateString()}
+        </p>
+      </div>
+      <p>{content}</p>
+    </div>
+  );
 };
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,4 +1,4 @@
-import {} from '../ReviewItem/ReviewItem';
+import { ReviewItem } from '../ReviewItem/ReviewItem';
 import { searchMovieReviews } from '../../services/api';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -9,20 +9,37 @@ export default function Reviews() {
   const [error, setError] = useState(null);
   const [reviews, setReviews] = useState([]);
 
-  // useEffect(() => {
-  //     const fetchDetails = async () => {
-  //       try {
-  //         setIsLoading(true);
-  //         setError(null);
-  //         const details = await searchMovieReviews(Number(movieId));
-  //         setReviews(reviews)
-  //       } catch (error) {
-  //         setError(error);
-  //       } finally {
-  //         setIsLoading(false);
-  //       }
-  //     };
-  //     fetchDetails();
-  //   }, [movieId]);
-  return <div>Reviews</div>;
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+        const details = await searchMovieReviews(Number(movieId));
+        setReviews(details.results);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchDetails();
+  }, [movieId]);
+  return (
+    <section>
+      <h2>Movie reviews</h2>
+      <ul>
+        {reviews.map(review => (
+          <li key={review.id}>
+            <ReviewItem review={review} />
+          </li>
+        ))}
+      </ul>
+      {isLoading && <div>LOADING...</div>}
+      {error && (
+        <div>
+          Something went wrong.. {error.message}. Please, reload the page!
+        </div>
+      )}
+    </section>
+  );
 }
